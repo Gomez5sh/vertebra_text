b4b70f967517e0f5a224b3295a5386b580b3561e
g5sh <eagomez2387@gmail.com>
2020-12-21 23:27:56 -0500

    [ADD]: Add the final backend

--- src/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const URI = "mongodb://localhost/crud-api";
-
-mongoose
-    .connect(URI)
-    .then((db) => console.log("DB is ON"))
-    .catch((err) => console.log(err));
-
-module.exports = mongoose;
