42cda6b199c27bdb4473b500605ec2c11adc23da
g5sh <eagomez2387@gmail.com>
2020-12-22 13:45:41 -0500

    [ADD]: Post modal

--- client/src/components/products.js
+++ client/src/components/products.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
 
 const url = "http://localhost:8080/api/product";
 
 class Products extends Component {
   state = {
     data: [],
+    modalInsert: false,
+    form: {
+      id: "",
+      name: "",
+      Sku: "",
+      unit_measurement: "",
+      unidad_quantity: "",
+      refrigerated: "",
+      suppliers_ID: "",
+    },
   };
 
   getPetition = () => {
@@ -15,6 +26,20 @@ class Products extends Component {
         data: response.data,
       });
     });
+  };
+
+  modalInsert = () => {
+    this.setState({ modalInsert: !this.state.modalInsert });
+  };
+
+  handleChange = async (e) => {
+    e.persist();
+    await this.setState({
+      // Heredar atributos que el usuario escriba
+      ...this.state.form,
+      [e.target.name]: e.target.value,
+    });
+    console.log(this.state.form);
   };
 
   componentDidMount() {
@@ -41,7 +66,12 @@ class Products extends Component {
                       <th>Refrigerated</th>
                       <th>Suppliers</th>
                       <th>
-                        <button className="btn btn-success">Create</button>
+                        <button
+                          className="btn btn-success"
+                          onClick={() => this.modalInsert()}
+                        >
+                          Create
+                        </button>
                       </th>
                     </tr>
                   </thead>
@@ -65,6 +95,125 @@ class Products extends Component {
                     })}
                   </tbody>
                 </table>
+
+                <Modal isOpen={this.state.modalInsert}>
+                  <ModalHeader style={{ display: "block" }}>
+                    <span
+                      style={{ float: "right" }}
+                      onClick={() => this.modalInsert()}
+                    >
+                      x
+                    </span>
+                  </ModalHeader>
+                  <ModalBody>
+                    <div className="form-group">
+                      <label htmlFor="id">ID</label>
+                      <input
+                        className="form-control"
+                        type="text"
+                        name="id"
+                        id="id"
+                        readOnly
+                        onChange={this.handleChange}
+                        //value={form ? form.id : this.state.data.length + 1}
+                      />
+                      <br />
+                      <label htmlFor="name">Name</label>
+                      <input
+                        className="form-control"
+                        type="text"
+                        name="nombre"
+                        id="nombre"
+                        onChange={this.handleChange}
+                        //value={form ? form.nombre : ""}
+                      />
+                      <br />
+                      <label htmlFor="description">Description</label>
+                      <input
+                        className="form-control"
+                        type="text"
+                        name="description"
+                        id="description"
+                        onChange={this.handleChange}
+                        //value={form ? form.pais : ""}
+                      />
+                      <br />
+                      <label htmlFor="sku">Sku</label>
+                      <input
+                        className="form-control"
+                        type="text"
+                        name="sku"
+                        id="sku"
+                        onChange={this.handleChange}
+                        //value={form ? form.capital_bursatil : ""}
+                      />
+                      <br />
+                      <label htmlFor="unit_measurement">Unit Measurement</label>
+                      <input
+                        className="form-control"
+                        type="text"
+                        name="unit_measurement"
+                        id="unit_measurement"
+                        onChange={this.handleChange}
+                        //value={form ? form.nombre : ""}
+                      />
+                      <br />
+                      <label htmlFor="unidad_quantity">Unidad Quantity</label>
+                      <input
+                        className="form-control"
+                        type="text"
+                        name="unidad_quantity"
+                        id="unidad_quantity"
+                        onChange={this.handleChange}
+                        //value={form ? form.nombre : ""}
+                      />
+                      <br />
+                      <label htmlFor="refrigerated">Refrigerated</label>
+                      <input
+                        className="form-control"
+                        type="text"
+                        name="refrigerated"
+                        id="refrigerated"
+                        onChange={this.handleChange}
+                        //value={form ? form.nombre : ""}
+                      />
+                      <br />
+                      <label htmlFor="suppliers_ID">Suppliers</label>
+                      <input
+                        className="form-control"
+                        type="text"
+                        name="suppliers_ID"
+                        id="suppliers_ID"
+                        onChange={this.handleChange}
+                        //value={form ? form.nombre : ""}
+                      />
+                    </div>
+                  </ModalBody>
+
+                  <ModalFooter>
+                    {this.state.tipoModal === "insertar" ? (
+                      <button
+                        className="btn btn-success"
+                        onClick={() => this.peticionPost()}
+                      >
+                        Insertar
+                      </button>
+                    ) : (
+                      <button
+                        className="btn btn-primary"
+                        onClick={() => this.peticionPut()}
+                      >
+                        Actualizar
+                      </button>
+                    )}
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => this.modalInsertar()}
+                    >
+                      Cancelar
+                    </button>
+                  </ModalFooter>
+                </Modal>
               </div>
             </div>
           </div>
